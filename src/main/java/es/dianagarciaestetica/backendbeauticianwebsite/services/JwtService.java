9ac2fedbd7619ad84e85c7151720a8f9eddf1758
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-12-09 01:16:35 +0100

    Update token duration

--- src/main/java/es/dianagarciaestetica/backendbeauticianwebsite/services/JwtService.java
+++ src/main/java/es/dianagarciaestetica/backendbeauticianwebsite/services/JwtService.java
@@ -24,12 +24,13 @@ public class JwtService {
     }
 
     private String getToken(Map<String, Object> extraClaims, UserDetails user) {
+    	Date currentDate = new Date();
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(user.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .setIssuedAt(currentDate)
+                .setExpiration(new Date(currentDate.getTime() + 86400000))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
