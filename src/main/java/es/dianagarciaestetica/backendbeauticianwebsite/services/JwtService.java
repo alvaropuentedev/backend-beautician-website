20adfe0c78f8fd169230981679930667f66e4f4b
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-12-17 03:17:19 +0100

    Jwt working

--- src/main/java/es/dianagarciaestetica/backendbeauticianwebsite/services/JwtService.java
+++ src/main/java/es/dianagarciaestetica/backendbeauticianwebsite/services/JwtService.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +17,8 @@ import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
-	@Value("${JWT_SECRET_KEY}")
-    private static String secretKey;
-	
+
+    private static final String SECRET_KEY = "3423784623847235E97R979TD98VDF6DGDF9GDF97VD9VF90VD79D0VF90VD0FVV0D";
     public String getToken(UserDetails user) {
         return getToken(new HashMap<>(), user);
     }
@@ -38,7 +36,7 @@ public class JwtService {
     }
 
     private Key getKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
